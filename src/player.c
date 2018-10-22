9034a054d231804f87a4dc2ce84977f14e436120
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2018-10-22 21:21:19 +0100

    [player] prev/next at the start/end of Q will wrap around like most CD players instead of error

--- src/player.c
+++ src/player.c
@@ -922,8 +922,23 @@ source_prev()
     }
 
   queue_item = db_queue_fetch_prev(cur_streaming->item_id, shuffle);
-  if (!queue_item)
-    return NULL;
+  if (!queue_item) 
+    {
+      int qsz;
+      int pos = db_queue_get_pos(cur_streaming->item_id, 0);
+ 
+      // do we have no prev item because we're the first item?
+      if ( pos != 0)
+        return NULL;
+
+      qsz = db_queue_get_count();
+      if (qsz < 0)
+        return NULL;
+
+      queue_item = db_queue_fetch_bypos(qsz-1, shuffle);
+      if (!queue_item)
+        return NULL;
+    }
 
   ps = source_new(queue_item);
   free_queue_item(queue_item, 0);
