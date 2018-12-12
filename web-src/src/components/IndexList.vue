f703747bb3b833b2c4a1f0891ed83e9c1e046f84
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2018-12-12 19:20:48 +0000

    ray: [web-src] horizontal-scrollable index'd navigation

---
+++ web-src/src/components/IndexList.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="section fd-tabs-bottom-section">
+    <div class="container">
+      <div class="columns is-centered">
+        <div class="column is-four-fifths">
+          <div class="tabs is-centered is-small">
+            <ul>
+              <list-idx-nav v-for="idx in filtered_index" :key="idx" :name="idx" :anchor="idx"></list-idx-nav>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import ListIdxNav from '@/components/ListIdxNav'
+
+export default {
+  name: 'IndexList',
+  components: { ListIdxNav },
+
+  props: ['index'],
+
+  computed: {
+    filtered_index () {
+      const specialChars = '!"#$%&\'()*+,-./:;<=>?@[\\]^`{|}~'
+      return this.index.filter(c => !specialChars.includes(c))
+    }
+  }
+}
+</script>
+
+<style>
+</style>
