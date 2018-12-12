1159e5c14826d6031c370ee884dd71198e1caf2b
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2018-12-12 19:20:48 +0000

    ray: [web-src] horizontal-scrollable index'd navigation

--- web-src/src/pages/PageAlbums.vue
+++ web-src/src/pages/PageAlbums.vue
@@ -78,6 +78,7 @@ export default {
 
   data () {
     return {
+      index_list: [],
       tracks: 0,
       albums: { items: [] },
       sort_options: ['Name', 'Recently added', 'Recently released']
--- web-src/src/pages/PageArtists.vue
+++ web-src/src/pages/PageArtists.vue
@@ -85,6 +85,12 @@ export default {
   },
 
   computed: {
+    index_list () {
+      return [...new Set(this.artists.items
+        .filter(artist => !this.$store.state.hide_singles || artist.track_count > (artist.album_count * 2))
+        .map(artist => artist.name_sort.charAt(0).toUpperCase()))]
+    },
+
     artists_list () {
       return new Artists(this.artists.items, {
         hideSingles: this.hide_singles,
