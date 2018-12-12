2f63c8834ec02692e4d3dc83965518b265e01ae9
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2018-12-12 19:20:48 +0000

    ray: [web-src] horizontal-scrollable index'd navigation

---
+++ web-src/src/components/ListIdxNav.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <a href="#" v-scroll-to="{ el: '#index_' + anchor, offset: -80 }">{{ name }}</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListIdxNav',
+
+  props: [ 'name', 'anchor' ]
+}
+</script>
+
+<style>
+</style>
