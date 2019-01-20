6a4f2905891424bbda99d374183754870db0169f
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2019-01-20 20:03:24 +0000

    [web-src] sync with jsonapi for handling albums compilations

--- web-src/src/pages/PageArtist.vue
+++ web-src/src/pages/PageArtist.vue
@@ -1,23 +1,20 @@
 <template>
-  <div>
-    <tabs-music></tabs-music>
-
-    <index-list :index="index_list"></index-list>
-
-    <content-with-heading>
-      <template slot="heading-left">
-        <p class="title is-4">{{ artist.name }}</p>
-      </template>
-      <template slot="heading-right">
+  <content-with-heading>
+    <template slot="heading-left">
+      <p class="title is-4">{{ album_artist }}</p>
+    </template>
+    <template slot="heading-right">
+      <div class="buttons is-centered">
         <a class="button is-small is-light is-rounded" @click="show_artist_details_modal = true">
           <span class="icon"><i class="mdi mdi-dots-horizontal mdi-18px"></i></span>
         </a>
         <a class="button is-small is-dark is-rounded" @click="play">
           <span class="icon"><i class="mdi mdi-shuffle"></i></span> <span>Shuffle</span>
         </a>
-      </template>
-      <template slot="content">
-        <p class="heading has-text-centered-mobile">{{ artist.album_count }} albums | <a class="has-text-link" @click="open_tracks">{{ artist.track_count }} tracks</a></p>
+      </div>
+    </template>
+    <template slot="content">
+      <p class="heading has-text-centered-mobile">{{ albums.total }} albums | <a class="has-text-link" @click="open_tracks">{{ track_count }} tracks</a></p>
       <list-item-album v-for="album in albums.items" :key="album.id" :album="album" @click="open_album(album)">
         <template slot="actions">
           <a @click="open_dialog(album)">
@@ -27,19 +24,16 @@
       </list-item-album>
       <modal-dialog-album :show="show_details_modal" :album="selected_album" @close="show_details_modal = false" />
       <modal-dialog-artist :show="show_artist_details_modal" :artist="artist" @close="show_artist_details_modal = false" />
-      </template>
-    </content-with-heading>
-  </div>
+    </template>
+  </content-with-heading>
 </template>
 
 <script>
 import { LoadDataBeforeEnterMixin } from './mixin'
 import ContentWithHeading from '@/templates/ContentWithHeading'
-import TabsMusic from '@/components/TabsMusic'
 import ListItemAlbum from '@/components/ListItemAlbum'
 import ModalDialogAlbum from '@/components/ModalDialogAlbum'
 import ModalDialogArtist from '@/components/ModalDialogArtist'
-import IndexList from '@/components/IndexList'
 import webapi from '@/webapi'
 
 const artistData = {
@@ -51,7 +45,9 @@ const artistData = {
   },
 
   set: function (vm, response) {
-    vm.artist = response[0].data
+    vm.album_artist = response[0].data.artist
+    vm.artist_id = vm.$route.params.artist_id
+    vm.artist = response[0].data.items
     vm.albums = response[1].data
   }
 }
@@ -59,12 +55,15 @@ const artistData = {
 export default {
   name: 'PageArtist',
   mixins: [ LoadDataBeforeEnterMixin(artistData) ],
-  components: { ContentWithHeading, TabsMusic, IndexList, ListItemAlbum, ModalDialogAlbum, ModalDialogArtist },
+  components: { ContentWithHeading, ListItemAlbum, ModalDialogAlbum, ModalDialogArtist },
 
   data () {
     return {
+      album_artist: '',
+      artist_id: '',
       artist: {},
       albums: { items: [] },
+
       show_details_modal: false,
       selected_album: {},
 
@@ -73,15 +72,18 @@ export default {
   },
 
   computed: {
-    index_list () {
-      return [...new Set(this.albums.items
-        .map(album => album.name_sort.charAt(0).toUpperCase()))]
+    track_count () {
+      var n = 0
+      return this.albums.items.reduce((acc, item) => {
+        acc += item.track_count
+        return acc
+      }, n)
     }
   },
 
   methods: {
     open_tracks: function () {
-      this.$router.push({ path: '/music/artists/' + this.artist.id + '/tracks' })
+      this.$router.push({ path: '/music/artists/' + this.artist_id + '/tracks' })
     },
 
     play: function () {
