12ad312c5364cbac77d5222fb77050a1aa9934d9
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2019-01-20 20:03:24 +0000

    [web-src] sync with jsonapi for handling albums compilations

--- web-src/src/pages/PageArtistTracks.vue
+++ web-src/src/pages/PageArtistTracks.vue
@@ -5,7 +5,7 @@
         <index-button-list :index="index_list"></index-button-list>
       </template>
       <template slot="heading-left">
-        <p class="title is-4">{{ artist.name }}</p>
+        <p class="title is-4">{{ album_artist }}</p>
       </template>
       <template slot="heading-right">
         <div class="buttons is-centered">
@@ -43,6 +43,8 @@ const tracksData = {
   },
 
   set: function (vm, response) {
+    vm.name = response[0].data.name
+    vm.id = response[0].data.id
     vm.artist = response[0].data
     vm.tracks = response[1].data.tracks
   }
@@ -55,6 +57,8 @@ export default {
 
   data () {
     return {
+      album_artist: '',
+      artist_id: '',
       artist: {},
       tracks: { items: [] },
 
@@ -63,6 +67,14 @@ export default {
   },
 
   computed: {
+    track_count () {
+      return this.tracks.items.length
+    },
+
+    album_count () {
+      return new Set(this.tracks.items.map(track => track.album_id)).size
+    },
+
     index_list () {
       return [...new Set(this.tracks.items
         .map(track => track.title_sort.charAt(0).toUpperCase()))]
@@ -76,7 +88,7 @@ export default {
   methods: {
     open_artist: function () {
       this.show_details_modal = false
-      this.$router.push({ path: '/music/artists/' + this.artist.id })
+      this.$router.push({ path: '/music/artists/' + this.artist_id })
     },
 
     play: function () {
