f98c51b2d7070a2fa8368c638232914b25d4ae2c
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2019-01-29 23:48:35 +0000

    [db] v21.1 add sontrackartistid column

--- src/db.h
+++ src/db.h
@@ -242,6 +242,7 @@ struct media_file_info {
   uint32_t tv_season_num;
 
   int64_t songartistid;
+  int64_t songtrackartistid;
   int64_t songalbumid;
 
   char *title_sort;
@@ -424,6 +425,7 @@ struct db_media_file_info {
   char *album_sort;
   char *album_artist_sort;
   char *composer_sort;
+  char *songtrackartistid;
   char *channels;
   char *usermark;
   char *scan_kind;
