0d80b3f8a20919b847a6bb2419e173cbd2a2d5a8
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2019-02-19 19:30:26 +0000

    [player] next/prev in STOP state handling

--- src/httpd_jsonapi.c
+++ src/httpd_jsonapi.c
@@ -1558,18 +1558,19 @@ static int
 jsonapi_reply_player_next(struct httpd_request *hreq)
 {
   int ret;
+  struct player_status status;
 
   ret = player_playback_next();
   if (ret < 0)
     {
-      struct player_status status;
-      player_get_status(&status);
-      if (status.status == PLAY_STOPPED)
-        return HTTP_NOCONTENT;
-
       DPRINTF(E_LOG, L_WEB, "Error switching to next item.\n");
       return HTTP_INTERNAL;
     }
+
+  player_get_status(&status);
+  if (status.status == PLAY_STOPPED)
+    // in the non-playing state, we just move the playhead
+    return HTTP_NOCONTENT;
 
   ret = player_playback_start();
   if (ret < 0)
@@ -1585,18 +1586,19 @@ static int
 jsonapi_reply_player_previous(struct httpd_request *hreq)
 {
   int ret;
+  struct player_status status;
 
   ret = player_playback_prev();
   if (ret < 0)
     {
-      struct player_status status;
-      player_get_status(&status);
-      if (status.status == PLAY_STOPPED)
-        return HTTP_NOCONTENT;
-
       DPRINTF(E_LOG, L_WEB, "Error switching to previous item.\n");
       return HTTP_INTERNAL;
     }
+
+  player_get_status(&status);
+  if (status.status == PLAY_STOPPED)
+    // in the non-playing state, we just move the playhead
+    return HTTP_NOCONTENT;
 
   ret = player_playback_start();
   if (ret < 0)
