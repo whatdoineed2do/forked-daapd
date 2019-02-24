32f9bd934aa86640be2209006dab29ccf6b9960b
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2019-02-24 21:04:59 +0000

    [player/httpd_jsonapi] next/prev in STOPPED state handling
    Allow user to move 'play head' to next/prev in STOPPED state but does not start
    playback; moving playhead past head/end end of queue will 'wraparound'
    
    Behaviour similar to that of many (physical) CD-players

--- src/httpd_jsonapi.c
+++ src/httpd_jsonapi.c
@@ -1489,6 +1489,7 @@ static int
 jsonapi_reply_player_next(struct httpd_request *hreq)
 {
   int ret;
+  struct player_status status;
 
   ret = player_playback_next();
   if (ret < 0)
@@ -1496,6 +1497,11 @@ jsonapi_reply_player_next(struct httpd_request *hreq)
       DPRINTF(E_LOG, L_WEB, "Error switching to next item.\n");
       return HTTP_INTERNAL;
     }
+
+  player_get_status(&status);
+  if (status.status == PLAY_STOPPED)
+    // in the non-playing state, we just move the playhead
+    return HTTP_NOCONTENT;
 
   ret = player_playback_start();
   if (ret < 0)
@@ -1511,6 +1517,7 @@ static int
 jsonapi_reply_player_previous(struct httpd_request *hreq)
 {
   int ret;
+  struct player_status status;
 
   ret = player_playback_prev();
   if (ret < 0)
@@ -1518,6 +1525,11 @@ jsonapi_reply_player_previous(struct httpd_request *hreq)
       DPRINTF(E_LOG, L_WEB, "Error switching to previous item.\n");
       return HTTP_INTERNAL;
     }
+
+  player_get_status(&status);
+  if (status.status == PLAY_STOPPED)
+    // in the non-playing state, we just move the playhead
+    return HTTP_NOCONTENT;
 
   ret = player_playback_start();
   if (ret < 0)
