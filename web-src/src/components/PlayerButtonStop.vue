134801c063e4427c40411c1de7dd301cc4f4ae74
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2019-03-17 15:40:04 +0000

    ray: [web-src] NowPlaying: add stop button to NavBarBottom
    ray: [web-src] add stop to NavBarBotttom

--- web-src/src/components/PlayerButtonStop.vue
+++ web-src/src/components/PlayerButtonStop.vue
@@ -1,6 +1,6 @@
 <template>
-  <a v-on:click="play_stop">
-    <span class="icon"><i class="mdi mdi-stop"></i></span>
+  <a @click="play_stop" :disabled="disabled">
+    <span class="icon"><i class="mdi mdi-stop" :class="icon_style"></i></span>
   </a>
 </template>
 
@@ -9,6 +9,16 @@ import webapi from '@/webapi'
 
 export default {
   name: 'PlayerButtonStop',
+
+  props: {
+    icon_style: String
+  },
+
+  computed: {
+    disabled () {
+      return !this.$store.state.queue || this.$store.state.queue.count <= 0
+    }
+  },
 
   methods: {
     play_stop: function () {
