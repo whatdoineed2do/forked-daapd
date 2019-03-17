44facc81fe76b9fef160b9b9a91e6b33ab31e87a
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2019-03-17 15:40:04 +0000

    ray: [web-src] NowPlaying: add stop button and reduce size of buttons

---
+++ web-src/src/components/PlayerButtonStop.vue
@@ -0,0 +1,22 @@
+<template>
+  <a v-on:click="play_stop">
+    <span class="icon"><i class="mdi mdi-stop"></i></span>
+  </a>
+</template>
+
+<script>
+import webapi from '@/webapi'
+
+export default {
+  name: 'PlayerButtonStop',
+
+  methods: {
+    play_stop: function () {
+      webapi.player_stop()
+    }
+  }
+}
+</script>
+
+<style>
+</style>
