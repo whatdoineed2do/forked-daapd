c368b6cf43f70bdaa751583d7c4bd862b4fde2a4
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2019-03-20 21:34:52 +0000

    [web-src] composer/composers view - supported at 'music tab' - supported at 'genres' to drill down composers for given genre - composer page shows albums/tracks

--- web-src/src/components/ModalDialogComposer.vue
+++ web-src/src/components/ModalDialogComposer.vue
@@ -7,26 +7,16 @@
           <div class="card">
             <div class="card-content">
               <p class="title is-4">
-                <a class="has-text-link" @click="open_composer">{{ composer.name }}</a>
+                <a class="has-text-link" @click="open_albums">{{ composer.name }}</a>
               </p>
-              <div class="content is-small">
-                <p>
-                  <span class="heading">Albums</span>
-                  <a class="title is-6 has-text-link" @click="open_albums">{{ composer.album_count }}</a>
-                </p>
-                <p>
-                  <span class="heading">Tracks</span>
-                  <a class="title is-6 has-text-link" @click="open_tracks">{{ composer.track_count }}</a>
-                </p>
-                <p>
-                  <span class="heading">Type</span>
-                  <span class="title is-6">{{ composer.data_kind }}</span>
-                </p>
-                <p>
-                  <span class="heading">Added at</span>
-                  <span class="title is-6">{{ composer.time_added | time('L LT') }}</span>
-                </p>
-              </div>
+              <p>
+                <span class="heading">Albums</span>
+                <a class="has-text-link is-6" @click="open_albums">{{ composer.album_count }}</a>
+              </p>
+              <p>
+                <span class="heading">Tracks</span>
+                <a class="has-text-link is-6" @click="open_tracks">{{ composer.track_count }}</a>
+              </p>
             </div>
             <footer class="card-footer">
               <a class="card-footer-item has-text-dark" @click="queue_add">
@@ -57,32 +47,27 @@ export default {
   methods: {
     play: function () {
       this.$emit('close')
-      webapi.player_play_uri(this.composer.uri, false)
+      webapi.player_play_expression('composer is "' + this.composer.name + '" and media_kind is music', false)
     },
 
     queue_add: function () {
       this.$emit('close')
-      webapi.queue_add(this.composer.uri)
+      webapi.queue_expression_add('composer is "' + this.composer.name + '" and media_kind is music')
     },
 
     queue_add_next: function () {
       this.$emit('close')
-      webapi.queue_add_next(this.composer.uri)
-    },
-
-    open_composer: function () {
-      this.$emit('close')
-      this.$router.push({ path: '/music/composers/' + this.composer.id })
+      webapi.queue_expression_add_next('composer is "' + this.composer.name + '" and media_kind is music')
     },
 
     open_albums: function () {
       this.$emit('close')
-      this.$router.push({ path: '/music/composers/' + this.composer.id + '/ablums' })
+      this.$router.push({ name: 'ComposerAlbums', params: { composer: this.composer.name } })
     },
 
     open_tracks: function () {
-      this.$emit('close')
-      this.$router.push({ path: '/music/composers/' + this.composer.id + '/tracks' })
+      this.show_details_modal = false
+      this.$router.push({ name: 'ComposerTracks', params: { composer: this.composer.name } })
     }
   }
 }
