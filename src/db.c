d77c944df0e845cdc573446e369346195e2c6126
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2019-04-04 09:18:40 +0100

    ray: [db,conf] cfg'rable sqlite extn location via db_extn

--- src/db.c
+++ src/db.c
@@ -6811,6 +6811,7 @@ db_open(void)
   char *journal_mode;
   int synchronous;
   int mmap_size;
+  char *dbextnso;
 
   ret = sqlite3_open(db_path, &hdl);
   if (ret != SQLITE_OK)
@@ -6830,8 +6831,10 @@ db_open(void)
       return -1;
     }
 
+  dbextnso = cfg_getstr(cfg_getsec(cfg, "general"), "db_extn");
+  DPRINTF(E_DBG, L_DB, "Database extn: %s\n", dbextnso);
   errmsg = NULL;
-  ret = sqlite3_load_extension(hdl, PKGLIBDIR "/forked-daapd-sqlext.so", NULL, &errmsg);
+  ret = sqlite3_load_extension(hdl, dbextnso, NULL, &errmsg);
   if (ret != SQLITE_OK)
     {
       if (errmsg)
