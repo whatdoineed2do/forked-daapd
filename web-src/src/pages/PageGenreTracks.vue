f043bbce89c7a2149f4986be14014bfa90b32228
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2019-04-07 12:16:02 +0100

    [web-src] *tracks pages can filter based on ratings

--- web-src/src/pages/PageGenreTracks.vue
+++ web-src/src/pages/PageGenreTracks.vue
@@ -11,6 +11,12 @@
       </template>
       <template slot="heading-right">
         <div class="buttons is-centered">
+          <star-rating v-model="min_rating"
+            :star-size="17"
+            :show-rating="false"
+            :max-rating="5"
+            :increment="0.5"
+            @rating-selected="show_rating"></star-rating>
           <a class="button is-small is-light is-rounded" @click="show_genre_details_modal = true">
             <span class="icon"><i class="mdi mdi-dots-horizontal mdi-18px"></i></span>
           </a>
@@ -20,8 +26,9 @@
         </div>
       </template>
       <template slot="content">
-        <p class="heading has-text-centered-mobile"><a class="has-text-link" @click="open_genre">albums</a> | {{ tracks.total }} tracks</p>
-        <list-tracks :tracks="tracks.items" :expression="expression"></list-tracks>
+        <p class="heading has-text-centered-mobile"><a class="has-text-link" @click="open_artists">artists</a> | <a class="has-text-link" @click="open_albums">albums</a> | {{ tracks.total }} tracks | <a class="has-text-link" @click="open_composers">composers</a></p>
+        <list-tracks :tracks="rated_tracks"></list-tracks>
+        <modal-dialog-track :show="show_details_modal" :track="selected_track" @close="show_details_modal = false" />
         <modal-dialog-genre :show="show_genre_details_modal" :genre="{ 'name': genre }" @close="show_genre_details_modal = false" />
       </template>
     </content-with-heading>
@@ -33,8 +40,10 @@ import { LoadDataBeforeEnterMixin } from './mixin'
 import ContentWithHeading from '@/templates/ContentWithHeading'
 import ListTracks from '@/components/ListTracks'
 import TabsMusic from '@/components/TabsMusic'
+import ModalDialogTrack from '@/components/ModalDialogTrack'
 import ModalDialogGenre from '@/components/ModalDialogGenre'
 import IndexList from '@/components/IndexList'
+import StarRating from 'vue-star-rating'
 import webapi from '@/webapi'
 
 const tracksData = {
@@ -51,12 +60,17 @@ const tracksData = {
 export default {
   name: 'PageGenreTracks',
   mixins: [LoadDataBeforeEnterMixin(tracksData)],
-  components: { ContentWithHeading, ListTracks, ModalDialogGenre, IndexList, TabsMusic },
+  components: { ContentWithHeading, ListTracks, ModalDialogGenre, ModalDialogTrack, IndexList, TabsMusic, StarRating },
 
   data () {
     return {
       tracks: { items: [] },
       genre: '',
+
+      min_rating: 0,
+
+      show_details_modal: false,
+      selected_track: {},
 
       show_genre_details_modal: false
     }
@@ -70,17 +84,52 @@ export default {
 
     expression () {
       return 'genre is "' + this.genre + '" and media_kind is music'
+    },
+
+    rated_tracks () {
+      return this.tracks.items.filter(track => track.rating >= this.min_rating)
     }
   },
 
   methods: {
-    open_genre: function () {
+    open_albums: function () {
       this.show_details_modal = false
       this.$router.push({ name: 'Genre', params: { genre: this.genre } })
     },
 
+    open_toptracks: function () {
+      this.show_details_modal = false
+      this.$router.push({ name: 'TopGenreTracks', params: { condition: 'genre is "' + this.genre + '" and media_kind is music', id: this.genre } })
+    },
+
+    open_composers: function () {
+      this.show_details_modal = false
+      this.$router.push({ name: 'Composers', params: { genre: this.genre } })
+    },
+
+    open_artists: function () {
+      this.show_details_modal = false
+      this.$router.push({ name: 'GenreArtists', params: { genre: this.genre } })
+    },
+
     play: function () {
       webapi.player_play_expression(this.expression, true)
+    },
+
+    play_track: function (position) {
+      webapi.player_play_expression(this.expression, false, position)
+    },
+
+    show_rating: function (rating) {
+      if (rating === 0.5) {
+        rating = 0
+      }
+      this.min_rating = Math.ceil(rating) * 20
+    },
+
+    open_dialog: function (track) {
+      this.selected_track = track
+      this.show_details_modal = true
     }
   }
 }
