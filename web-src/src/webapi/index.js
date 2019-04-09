43b3ba96581ed1f52177a51f6eb3762f7f4f18aa
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2019-04-09 19:41:47 +0100

    [web-src] add notification for rating update

--- web-src/src/webapi/index.js
+++ web-src/src/webapi/index.js
@@ -381,6 +381,13 @@ export default {
     return axios.put('./api/library/tracks/' + trackId, undefined, { params: attributes })
   },
 
+  library_track_set_rating (trackId, rating) {
+    return axios.put('/api/library/tracks/' + trackId, undefined, { params: { rating: rating } }).then((response) => {
+      store.dispatch('add_notification', { text: 'track rating updated', type: 'info', timeout: 1500 })
+      return Promise.resolve(response)
+    })
+  },
+
   library_files (directory = undefined) {
     const filesParams = { directory: directory }
     return axios.get('./api/library/files', {
