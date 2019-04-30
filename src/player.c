94ac7d4aa5bbfc9854cf025e57093d69c51a78f4
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2019-04-30 12:19:51 +0100

    [transcode] review cleanup for bitrate/input codec

--- src/player.c
+++ src/player.c
@@ -1701,17 +1701,6 @@ get_status(void *arg, int *retval)
 
   status->plid = cur_plid;
 
-  if (pb_session.playing_now == NULL)
-    memset(&status->quality, 0, sizeof(struct media_quality));
-  else
-    {
-      status->quality.sample_rate     = pb_session.playing_now->quality.sample_rate;
-      status->quality.bits_per_sample = pb_session.playing_now->quality.bits_per_sample;
-      status->quality.channels        = pb_session.playing_now->quality.channels;
-      status->quality.bit_rate        = pb_session.playing_now->quality.bit_rate;
-      status->quality.codec           = pb_session.playing_now->quality.codec;
-    }
-
   switch (player_state)
     {
       case PLAY_STOPPED:
@@ -1724,6 +1713,7 @@ get_status(void *arg, int *retval)
             status->item_id = pb_session.playing_now->item_id;
             status->pos_ms  = 0;
             status->len_ms  = pb_session.playing_now->len_ms;
+            status->quality = pb_session.playing_now->quality;
           }
 
 	break;
