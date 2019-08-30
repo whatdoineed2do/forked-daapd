5d559b6730a03b70024264caf21bed825067f587
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2019-08-30 23:33:32 +0100

    Revert "ray: [streaming] public _request to be consistent with other httpd _request methods returning void"
    This reverts commit ba500bb461bf06a9775987042eda30b5d394484b.

--- src/httpd_streaming.c
+++ src/httpd_streaming.c
@@ -523,7 +523,7 @@ streaming_write(struct output_buffer *obuf)
     }
 }
 
-void
+int
 streaming_request(struct evhttp_request *req, struct httpd_uri_parsed *uri_parsed)
 {
   struct streaming_session *session;
@@ -542,7 +542,7 @@ streaming_request(struct evhttp_request *req, struct httpd_uri_parsed *uri_parse
       DPRINTF(E_LOG, L_STREAMING, "Got MP3 streaming request, but cannot encode to MP3\n");
 
       evhttp_send_error(req, HTTP_NOTFOUND, "Not Found");
-      return;
+      return -1;
     }
 
   evcon = evhttp_request_get_connection(req);
@@ -580,7 +580,7 @@ streaming_request(struct evhttp_request *req, struct httpd_uri_parsed *uri_parse
       DPRINTF(E_LOG, L_STREAMING, "Out of memory for streaming request\n");
 
       evhttp_send_error(req, HTTP_SERVUNAVAIL, "Internal Server Error");
-      return;
+      return -1;
     }
 
   pthread_mutex_lock(&streaming_sessions_lck);
@@ -600,6 +600,8 @@ streaming_request(struct evhttp_request *req, struct httpd_uri_parsed *uri_parse
   pthread_mutex_unlock(&streaming_sessions_lck);
 
   evhttp_connection_set_closecb(evcon, streaming_close_cb, session);
+
+  return 0;
 }
 
 int
