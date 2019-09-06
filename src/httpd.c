7b7eb5a299d714997823a0d20620293cdc6e1515
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2019-09-06 17:18:28 +0100

    [httpd] remove reference to global evbase_httpd from protocol specific handlers, accept on _init() instead

--- src/httpd.c
+++ src/httpd.c
@@ -1684,7 +1684,7 @@ httpd_init(const char *webroot)
       goto rsp_fail;
     }
 
-  ret = daap_init();
+  ret = daap_init(evbase_httpd);
   if (ret < 0)
     {
       DPRINTF(E_FATAL, L_HTTPD, "DAAP protocol init failed\n");
@@ -1692,7 +1692,7 @@ httpd_init(const char *webroot)
       goto daap_fail;
     }
 
-  ret = dacp_init();
+  ret = dacp_init(evbase_httpd);
   if (ret < 0)
     {
       DPRINTF(E_FATAL, L_HTTPD, "DACP protocol init failed\n");
@@ -1734,7 +1734,7 @@ httpd_init(const char *webroot)
     }
 #endif
 
-  streaming_init();
+  streaming_init(evbase_httpd);
 
 #ifdef HAVE_EVENTFD
   exit_efd = eventfd(0, EFD_CLOEXEC);
