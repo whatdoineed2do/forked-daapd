1299f20fe6e72e206bdb6788d7771eb818202ea1
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2019-09-15 15:40:53 +0100

    ray: [raop] global 'airplay *' section

--- src/outputs/raop.c
+++ src/outputs/raop.c
@@ -341,6 +341,7 @@ static struct raop_session *raop_sessions;
 static int
 raop_device_start(struct output_device *rd, int callback_id);
 
+static bool exclude_all = false;
 
 /* ------------------------------- MISC HELPERS ----------------------------- */
 
@@ -4419,6 +4420,11 @@ raop_device_cb(const char *name, const char *type, const char *domain, const cha
   device_name++;
 
   DPRINTF(E_DBG, L_RAOP, "Event for AirPlay device '%s' (port %d, id %" PRIx64 ")\n", device_name, port, id);
+  if (exclude_all)
+    {
+      DPRINTF(E_LOG, L_RAOP, "Excluding all AirPlay devices, incl '%s' as set in config\n", device_name);
+      return;
+    }
 
   devcfg = cfg_gettsec(cfg, "airplay", device_name);
   if (devcfg && cfg_getbool(devcfg, "exclude"))
@@ -4915,6 +4921,8 @@ raop_init(void)
 
       goto out_stop_control;
     }
+
+  exclude_all = cfg_getbool(cfg_gettsec(cfg, "airplay", "*"), "exclude");
 
   return 0;
 
