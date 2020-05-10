4410f2e1c31b93e8eaa6f38e0f5793358432c997
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2020-05-10 17:37:16 +0100

    [scripts] support both files and playlists

--- scripts/fd-playlist-add.sh
+++ scripts/fd-playlist-add.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 usage() {
-  echo "usage: $(basename $0) [ -c ] [ -s ] <playlist location>"
+  echo "usage: $(basename $0) [ -c ] [ -s ] -f | -p <file|playlist location>"
   echo "       -c  clears current queue, default false"
   echo "       -s  start playback, default no"
 
@@ -12,12 +12,18 @@ FD_SVR=http://localhost:3689
 
 CLEAR="false"
 PLAYBACK="nah"
+TYPE=""
 
-while getopts "cs" opt; do
+while getopts "csfp" opt; do
   case ${opt} in
     c) CLEAR="true"
       ;;
     s) PLAYBACK="start"
+      ;;
+
+    p) TYPE="playlist"
+      ;;
+    f) TYPE="file"
       ;;
 
     *) usage; exit 1
@@ -26,13 +32,18 @@ done
 shift $((OPTIND -1))
 
 [ $# -ne 1 ] && usage && exit 1
+[ -z $TYPE ] && usage && exit 1
 
-PLAYLIST_PATH=$1
-[ ! -f ${PLAYLIST_PATH} ] && echo "no such playlist: ${PLAYLIST_PATH}" && exit 1
+FILEPLAYLIST_PATH=$1
+[ ! -f ${FILEPLAYLIST_PATH} ] && echo "no such ${TYPE}: ${FILEPLAYLIST_PATH}" && exit 1
 
-URI=$(curl -s -X GET ${FD_SVR}/api/library/playlists | jq -r ".items[] | select (.path == \"${PLAYLIST_PATH}\") | .uri")
+if [ "${TYPE}" == "playlist" ]; then
+  URI=$(curl -s -X GET ${FD_SVR}/api/library/playlists | jq -r ".items[] | select (.path == \"${FILEPLAYLIST_PATH}\") | .uri")
+elif [ "${TYPE}" == "file" ]; then
+  URI=$(curl -s -X GET "${FD_SVR}/api/search?type=track&expression=path+is+%22${FILEPLAYLIST_PATH}%22" | jq -r ".tracks | .items[].uri")
+fi
 
-[ -z $URI ] && echo "no such playlist found: $PLAYLIST_PATH" && exit 1
+[ -z $URI ] && echo "no such ${TYPE} found: $FILEPLAYLIST_PATH" && exit 1
 
 TRACKS=$(curl -s -X POST "${FD_SVR}/api/queue/items/add?clear=${CLEAR}&playback=${PLAYBACK}&uris=${URI}" | jq -r .count)
-echo "${PLAYLIST_PATH} => ${URI}  = ${TRACKS} tracks"
+echo "${FILEPLAYLIST_PATH} => ${URI}  = ${TRACKS} tracks"
