d1d1c6e626842284cf151ac1aa534fb9ab563c5e
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2021-01-16 21:38:20 +0000

    [jsonapi] artwork_url incls db_timestamp for versioning/caching invalidation for groups

--- src/httpd_jsonapi.c
+++ src/httpd_jsonapi.c
@@ -361,7 +361,7 @@ artist_to_json(struct db_group_info *dbgri)
   if (ret < sizeof(uri))
     json_object_object_add(item, "uri", json_object_new_string(uri));
 
-  ret = snprintf(artwork_url, sizeof(artwork_url), "./artwork/group/%s", dbgri->id);
+  ret = snprintf(artwork_url, sizeof(artwork_url), "./artwork/group/%s?v=%s", dbgri->id, dbgri->db_timestamp);
   if (ret < sizeof(artwork_url))
     json_object_object_add(item, "artwork_url", json_object_new_string(artwork_url));
 
@@ -412,7 +412,7 @@ album_to_json(struct db_group_info *dbgri)
   if (ret < sizeof(uri))
     json_object_object_add(item, "uri", json_object_new_string(uri));
 
-  ret = snprintf(artwork_url, sizeof(artwork_url), "./artwork/group/%s", dbgri->id);
+  ret = snprintf(artwork_url, sizeof(artwork_url), "./artwork/group/%s?v=%s", dbgri->id, dbgri->db_timestamp);
   if (ret < sizeof(artwork_url))
     json_object_object_add(item, "artwork_url", json_object_new_string(artwork_url));
 
