beee546020f51239d08497672d908569a5ca3c3d
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2021-01-24 13:35:50 +0000

    [db] *_ping_excl_bymatch() for partial scans

--- src/db.h
+++ src/db.h
@@ -618,6 +618,9 @@ db_file_ping_bypath(const char *path, time_t mtime_max);
 void
 db_file_ping_bymatch(const char *path, int isdir);
 
+void
+db_file_ping_excl_bymatch(const char *path);
+
 char *
 db_file_path_byid(int id);
 
@@ -681,6 +684,9 @@ db_pl_ping(int id);
 
 void
 db_pl_ping_bymatch(const char *path, int isdir);
+
+void
+db_pl_ping_excl_bymatch(const char *path);
 
 void
 db_pl_ping_items_bymatch(const char *path, int id);
@@ -759,6 +765,9 @@ db_directory_addorupdate(char *virtual_path, char *path, int disabled, int paren
 
 void
 db_directory_ping_bymatch(char *virtual_path);
+
+void
+db_directory_ping_excl_bymatch(char *virtual_path);
 
 void
 db_directory_disable_bymatch(char *path, enum strip_type strip, uint32_t cookie);
