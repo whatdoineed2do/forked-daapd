a0bec5aa2ec35d81501d8dde1926c1d982da315b
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2021-09-06 18:58:13 +0100

    [web-src] now playing modal setting track's usermark review status

--- web-src/src/components/ModalDialogQueueItem.vue
+++ web-src/src/components/ModalDialogQueueItem.vue
@@ -61,6 +61,16 @@
                     <span v-if="item.bitrate"> | {{ item.bitrate }} Kb/s</span>
                   </span>
                 </p>
+                <p>
+                  <span class="heading">Usermark</span>
+                  <span class="title is-6">{{ this.usermark }}</span>
+                </p>
+                <div class="buttons">
+                  <a class="button is-small is-danger" @click="usermark_update(1)">Mark to delete</a>
+                  <a class="button is-small is-warning" @click="usermark_update(2)">Mark to rexcode</a>
+                  <a class="button is-small is-warning" @click="usermark_update(4)">Mark to review</a>
+                  <a v-if="this.usermark > 0" class="button is-small is-success" @click="usermark_update(0)">Mark reset</a>
+                </div>
               </div>
             </div>
             <footer class="card-footer">
@@ -92,6 +102,7 @@ export default {
 
   data () {
     return {
+      usermark: -1,
       spotify_track: {}
     }
   },
@@ -142,6 +153,20 @@ export default {
     open_spotify_album: function () {
       this.$emit('close')
       this.$router.push({ path: '/music/spotify/albums/' + this.spotify_track.album.id })
+    },
+
+    usermark_update (value) {
+      const newvalue = value === 0 ? 0 : value | this.usermark
+      webapi.library_track_set_usermark(this.track_id, newvalue).then(() => {
+        this.usermark = newvalue
+      })
+    }
+  },
+
+  computed: {
+    track_id () {
+      const item = this.$store.state.queue.items.find((elem) => elem.id === this.item.id)
+      return (item === undefined) ? -1 : item.track_id
     }
   },
 
@@ -155,6 +180,11 @@ export default {
         })
       } else {
         this.spotify_track = {}
+        webapi.library_track(this.track_id).then((response) => {
+          this.usermark = response.data.usermark
+        }).catch(() => {
+          this.usermark = -1
+        })
       }
     }
   }
