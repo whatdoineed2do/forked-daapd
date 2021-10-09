4f5b4b4f8f51335217839c915142616756c8be47
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2021-10-09 15:36:27 +0100

    [scripts] sync remote server usermark data

--- scripts/fd-usermark.sh
+++ scripts/fd-usermark.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
 TRGT=/media/public/_delete
+
+
+# sync other server to main
+
+ALT_DATA=$(curl -s "http://rpi1:3689/api/search?type=tracks&expression=usermark+>+0" | jq -r ".tracks.items[] | select(.time_modified | fromdateiso8601 > $(date +%s --date="2days ago")) | {id, path, usermark}")
+readarray -t ALT_PATHS < <(echo $ALT_DATA | jq -r '.path')
+
+N=${#ALT_PATHS[@]}
+for ((i=0; i<${N}; i++)); do
+    TRACK_PATH=${ALT_PATHS[$i]}
+    TRACK_ID=$(curl -s -G --data-urlencode 'type=tracks' --data-urlencode "expression=path is \"${TRACK_PATH}\"" http://localhost:3689/api/search | jq -r '.tracks.items[0].id')
+    [ "$TRACK_ID" == "null" ] && continue
+
+    ALT_DATA=$(echo $ALT_DATA | jq --arg argpath "$TRACK_PATH" --arg argid $TRACK_ID 'if (.path == $argpath) then (.id=$argid) else . end')
+done
+curl -X PUT -d "$(echo $ALT_DATA | jq -s '{tracks: . }')" http://localhost:3689/api/library/tracks
+
+
 readarray -t DIRT < <(curl -s "http://localhost:3689/api/search?type=tracks&expression=usermark+>+0" | jq -r ".tracks.items[] | select(.time_modified | fromdateiso8601 < $(date +%s --date="5days ago")) | .path" )
-readarray -t DIRT1 < <(curl -s "http://rpi1:3689/api/search?type=tracks&expression=usermark+>+0" | jq -r ".tracks.items[] | select(.time_modified | fromdateiso8601 < $(date +%s --date="2days ago")) | .path" )
-DIRT+=("${DIRT1[@]}")
 
 N=${#DIRT[@]}
 if [ $N -eq 0 ]; then
