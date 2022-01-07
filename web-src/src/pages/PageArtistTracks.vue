3f65d25cbdc7dd489dfb9c59a7136aca9f0b06ab
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2022-01-07 19:28:23 +0000

    ray: [web-src] artist tracks supporting dynamic sorting

--- web-src/src/pages/PageArtistTracks.vue
+++ web-src/src/pages/PageArtistTracks.vue
@@ -2,11 +2,18 @@
   <div>
     <tabs-music></tabs-music>
 
-    <index-list :index="index_list"></index-list>
+    <index-list :index="tracks_list.indexList"></index-list>
 
     <content-with-heading>
       <template slot="options">
-        <index-button-list :index="index_list"></index-button-list>
+        <index-button-list :index="tracks_list.indexList"></index-button-list>
+
+        <div class="columns">
+          <div class="column">
+            <p class="heading" style="margin-bottom: 24px;">Sort by</p>
+            <dropdown-menu v-model="sort" :options="sort_options"></dropdown-menu>
+          </div>
+        </div>
       </template>
       <template slot="heading-left">
         <p class="title is-4">{{ name }}</p>
@@ -29,7 +36,7 @@
       </template>
       <template slot="content">
         <p class="heading has-text-centered-mobile"><a class="has-text-link" @click="open_artist">{{ album_count }} albums</a> | {{ track_count }} tracks</p>
-        <list-tracks :tracks="rated_tracks" @rating-updated="rating_upd" @usermark-updated="usermark_upd"></list-tracks>
+        <list-tracks :tracks="tracks_list" @rating-updated="rating_upd" @usermark-updated="usermark_upd"></list-tracks>
         <modal-dialog-track :show="show_details_modal" :track="selected_track" @close="show_details_modal = false" />
         <modal-dialog-artist :show="show_artist_details_modal" :artist="modal_artist_obj" @close="show_artist_details_modal = false" />
       </template>
@@ -41,6 +48,7 @@
 import { LoadDataBeforeEnterMixin } from './mixin'
 import ContentWithHeading from '@/templates/ContentWithHeading'
 import IndexButtonList from '@/components/IndexButtonList'
+import DropdownMenu from '@/components/DropdownMenu'
 import ListTracks from '@/components/ListTracks'
 import ModalDialogTrack from '@/components/ModalDialogTrack'
 import ModalDialogArtist from '@/components/ModalDialogArtist'
@@ -48,6 +56,8 @@ import TabsMusic from '@/components/TabsMusic'
 import IndexList from '@/components/IndexList'
 import StarRating from 'vue-star-rating'
 import webapi from '@/webapi'
+import * as types from '@/store/mutation_types'
+import Tracks from '@/lib/Tracks'
 
 const tracksData = {
   load: function (to) {
@@ -68,7 +78,7 @@ const tracksData = {
 export default {
   name: 'PageArtistTracks',
   mixins: [LoadDataBeforeEnterMixin(tracksData)],
-  components: { ContentWithHeading, ListTracks, IndexButtonList, ModalDialogArtist, ModalDialogTrack, IndexList, TabsMusic, StarRating },
+  components: { ContentWithHeading, ListTracks, IndexButtonList, DropdownMenu, ModalDialogArtist, ModalDialogTrack, IndexList, TabsMusic, StarRating },
 
   data () {
     return {
@@ -80,6 +90,7 @@ export default {
 
       show_details_modal: false,
       selected_track: {},
+      sort_options: ['Name', 'Recently added', 'Recently released'],
 
       show_artist_details_modal: false
     }
@@ -109,9 +120,21 @@ export default {
       }
     },
 
-    index_list () {
-      return [...new Set(this.tracks.items
-        .map(track => track.title_sort.charAt(0).toUpperCase()))]
+    tracks_list () {
+      return new Tracks(this.tracks.items, {
+        sort: this.sort,
+        group: true,
+        min_rating: this.min_rating
+      })
+    },
+
+    sort: {
+      get () {
+        return this.$store.state.albums_sort
+      },
+      set (value) {
+        this.$store.commit(types.ALBUMS_SORT, value)
+      }
     },
 
     track_uris () {
