6d3854eb21bdd52e9bb65af3a6d97e3343fb1ff8
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2022-04-26 08:17:05 +0100

    ray: [web-src] vue3 from upstream 42e708fb

---
+++ web-src.v3/src/components/NavbarTop.vue
@@ -0,0 +1,264 @@
+<template>
+  <nav
+    class="fd-top-navbar navbar is-light is-fixed-top"
+    :style="zindex"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div class="navbar-brand">
+      <navbar-item-link v-if="is_visible_playlists" to="/playlists">
+        <span class="icon"><mdicon name="music-box-multiple" size="16" /></span>
+      </navbar-item-link>
+      <navbar-item-link v-if="is_visible_music" to="/music">
+        <span class="icon"><mdicon name="music" size="16" /></span>
+      </navbar-item-link>
+      <navbar-item-link v-if="is_visible_podcasts" to="/podcasts">
+        <span class="icon"><mdicon name="microphone" size="16" /></span>
+      </navbar-item-link>
+      <navbar-item-link v-if="is_visible_audiobooks" to="/audiobooks">
+        <span class="icon"><mdicon name="book-open-variant" size="16" /></span>
+      </navbar-item-link>
+      <navbar-item-link v-if="is_visible_radio" to="/radio">
+        <span class="icon"><mdicon name="radio" size="16" /></span>
+      </navbar-item-link>
+      <navbar-item-link v-if="is_visible_files" to="/files">
+        <span class="icon"><mdicon name="folder-open" size="16" /></span>
+      </navbar-item-link>
+      <navbar-item-link v-if="is_visible_search" to="/search">
+        <span class="icon"><mdicon name="magnify" size="16" /></span>
+      </navbar-item-link>
+
+      <div
+        class="navbar-burger"
+        :class="{ 'is-active': show_burger_menu }"
+        @click="show_burger_menu = !show_burger_menu"
+      >
+        <span />
+        <span />
+        <span />
+      </div>
+    </div>
+
+    <div class="navbar-menu" :class="{ 'is-active': show_burger_menu }">
+      <div class="navbar-start" />
+
+      <div class="navbar-end">
+        <!-- Burger menu entries -->
+        <div
+          class="navbar-item has-dropdown is-hoverable"
+          :class="{ 'is-active': show_settings_menu }"
+          @click="on_click_outside_settings"
+        >
+          <a class="navbar-link is-arrowless">
+            <span class="icon is-hidden-touch"
+              ><mdicon name="menu" size="24"
+            /></span>
+            <span class="is-hidden-desktop has-text-weight-bold">OwnTone</span>
+          </a>
+
+          <div class="navbar-dropdown is-right">
+            <navbar-item-link to="/playlists">
+              <span class="icon"
+                ><mdicon name="music-box-multiple" size="16"
+              /></span>
+              <b>Playlists</b>
+            </navbar-item-link>
+            <navbar-item-link to="/music" exact>
+              <span class="icon"><mdicon name="music" size="16" /></span>
+              <b>Music</b>
+            </navbar-item-link>
+            <navbar-item-link to="/music/artists">
+              <span class="fd-navbar-item-level2">Artists</span>
+            </navbar-item-link>
+            <navbar-item-link to="/music/albums">
+              <span class="fd-navbar-item-level2">Albums</span>
+            </navbar-item-link>
+            <navbar-item-link to="/music/genres">
+              <span class="fd-navbar-item-level2">Genres</span>
+            </navbar-item-link>
+            <navbar-item-link v-if="spotify_enabled" to="/music/spotify">
+              <span class="fd-navbar-item-level2">Spotify</span>
+            </navbar-item-link>
+            <navbar-item-link to="/podcasts">
+              <span class="icon"><mdicon name="microphone" size="16" /></span>
+              <b>Podcasts</b>
+            </navbar-item-link>
+            <navbar-item-link to="/audiobooks">
+              <span class="icon"
+                ><mdicon name="book-open-variant" size="16"
+              /></span>
+              <b>Audiobooks</b>
+            </navbar-item-link>
+            <navbar-item-link to="/radio">
+              <span class="icon"><mdicon name="radio" size="16" /></span>
+              <b>Radio</b>
+            </navbar-item-link>
+            <navbar-item-link to="/files">
+              <span class="icon"><mdicon name="folder-open" size="16" /></span>
+              <b>Files</b>
+            </navbar-item-link>
+            <navbar-item-link to="/search">
+              <span class="icon"><mdicon name="magnify" size="16" /></span>
+              <b>Search</b>
+            </navbar-item-link>
+            <hr class="fd-navbar-divider" />
+
+            <navbar-item-link to="/settings/webinterface">
+              Settings
+            </navbar-item-link>
+            <a class="navbar-item" @click.stop.prevent="open_update_dialog()">
+              Update Library
+            </a>
+            <navbar-item-link to="/about"> About </navbar-item-link>
+
+            <div
+              class="navbar-item is-hidden-desktop"
+              style="margin-bottom: 2.5rem"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div
+      v-show="show_settings_menu"
+      class="is-overlay"
+      style="z-index: 10; width: 100vw; height: 100vh"
+      @click="show_settings_menu = false"
+    />
+  </nav>
+</template>
+
+<script>
+import NavbarItemLink from './NavbarItemLink.vue'
+import * as types from '@/store/mutation_types'
+
+export default {
+  name: 'NavbarTop',
+  components: { NavbarItemLink },
+
+  data() {
+    return {
+      show_settings_menu: false
+    }
+  },
+
+  computed: {
+    is_visible_playlists() {
+      return this.$store.getters.settings_option(
+        'webinterface',
+        'show_menu_item_playlists'
+      ).value
+    },
+    is_visible_music() {
+      return this.$store.getters.settings_option(
+        'webinterface',
+        'show_menu_item_music'
+      ).value
+    },
+    is_visible_podcasts() {
+      return this.$store.getters.settings_option(
+        'webinterface',
+        'show_menu_item_podcasts'
+      ).value
+    },
+    is_visible_audiobooks() {
+      return this.$store.getters.settings_option(
+        'webinterface',
+        'show_menu_item_audiobooks'
+      ).value
+    },
+    is_visible_radio() {
+      return this.$store.getters.settings_option(
+        'webinterface',
+        'show_menu_item_radio'
+      ).value
+    },
+    is_visible_files() {
+      return this.$store.getters.settings_option(
+        'webinterface',
+        'show_menu_item_files'
+      ).value
+    },
+    is_visible_search() {
+      return this.$store.getters.settings_option(
+        'webinterface',
+        'show_menu_item_search'
+      ).value
+    },
+
+    player() {
+      return this.$store.state.player
+    },
+
+    config() {
+      return this.$store.state.config
+    },
+
+    library() {
+      return this.$store.state.library
+    },
+
+    audiobooks() {
+      return this.$store.state.audiobooks_count
+    },
+
+    podcasts() {
+      return this.$store.state.podcasts_count
+    },
+
+    spotify_enabled() {
+      return this.$store.state.spotify.webapi_token_valid
+    },
+
+    show_burger_menu: {
+      get() {
+        return this.$store.state.show_burger_menu
+      },
+      set(value) {
+        this.$store.commit(types.SHOW_BURGER_MENU, value)
+      }
+    },
+
+    show_player_menu() {
+      return this.$store.state.show_player_menu
+    },
+
+    show_update_dialog: {
+      get() {
+        return this.$store.state.show_update_dialog
+      },
+      set(value) {
+        this.$store.commit(types.SHOW_UPDATE_DIALOG, value)
+      }
+    },
+
+    zindex() {
+      if (this.show_player_menu) {
+        return 'z-index: 20'
+      }
+      return ''
+    }
+  },
+
+  watch: {
+    $route(to, from) {
+      this.show_settings_menu = false
+    }
+  },
+
+  methods: {
+    on_click_outside_settings() {
+      this.show_settings_menu = !this.show_settings_menu
+    },
+
+    open_update_dialog() {
+      this.show_update_dialog = true
+      this.show_settings_menu = false
+      this.show_burger_menu = false
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ web-src.v3/src/pages/SpotifyPageBrowseNewReleases.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="fd-page-with-tabs">
+    <tabs-music />
+
+    <content-with-heading>
+      <template #heading-left>
+        <p class="title is-4">New Releases</p>
+      </template>
+      <template #content>
+        <spotify-list-item-album
+          v-for="album in new_releases"
+          :key="album.id"
+          :album="album"
+          @click="open_album(album)"
+        >
+          <template v-if="is_visible_artwork" #artwork>
+            <p class="image is-64x64 fd-has-shadow fd-has-action">
+              <cover-artwork
+                :artwork_url="artwork_url(album)"
+                :artist="album.artist"
+                :album="album.name"
+                :maxwidth="64"
+                :maxheight="64"
+              />
+            </p>
+          </template>
+          <template #actions>
+            <a @click.prevent.stop="open_album_dialog(album)">
+              <span class="icon has-text-dark"
+                ><mdicon name="dots-vertical" size="16"
+              /></span>
+            </a>
+          </template>
+        </spotify-list-item-album>
+        <spotify-modal-dialog-album
+          :show="show_album_details_modal"
+          :album="selected_album"
+          @close="show_album_details_modal = false"
+        />
+      </template>
+    </content-with-heading>
+  </div>
+</template>
+
+<script>
+import ContentWithHeading from '@/templates/ContentWithHeading.vue'
+import TabsMusic from '@/components/TabsMusic.vue'
+import SpotifyListItemAlbum from '@/components/SpotifyListItemAlbum.vue'
+import SpotifyModalDialogAlbum from '@/components/SpotifyModalDialogAlbum.vue'
+import CoverArtwork from '@/components/CoverArtwork.vue'
+import store from '@/store'
+import * as types from '@/store/mutation_types'
+import SpotifyWebApi from 'spotify-web-api-js'
+
+const dataObject = {
+  load: function (to) {
+    if (store.state.spotify_new_releases.length > 0) {
+      return Promise.resolve()
+    }
+
+    const spotifyApi = new SpotifyWebApi()
+    spotifyApi.setAccessToken(store.state.spotify.webapi_token)
+    return spotifyApi.getNewReleases({
+      country: store.state.spotify.webapi_country,
+      limit: 50
+    })
+  },
+
+  set: function (vm, response) {
+    if (response) {
+      store.commit(types.SPOTIFY_NEW_RELEASES, response.albums.items)
+    }
+  }
+}
+
+export default {
+  name: 'SpotifyPageBrowseNewReleases',
+  components: {
+    ContentWithHeading,
+    TabsMusic,
+    SpotifyListItemAlbum,
+    SpotifyModalDialogAlbum,
+    CoverArtwork
+  },
+
+  beforeRouteEnter(to, from, next) {
+    dataObject.load(to).then((response) => {
+      next((vm) => dataObject.set(vm, response))
+    })
+  },
+  beforeRouteUpdate(to, from, next) {
+    const vm = this
+    dataObject.load(to).then((response) => {
+      dataObject.set(vm, response)
+      next()
+    })
+  },
+
+  data() {
+    return {
+      show_album_details_modal: false,
+      selected_album: {}
+    }
+  },
+
+  computed: {
+    new_releases() {
+      return this.$store.state.spotify_new_releases
+    },
+
+    is_visible_artwork() {
+      return this.$store.getters.settings_option(
+        'webinterface',
+        'show_cover_artwork_in_album_lists'
+      ).value
+    }
+  },
+
+  methods: {
+    open_album: function (album) {
+      this.$router.push({ path: '/music/spotify/albums/' + album.id })
+    },
+
+    open_album_dialog: function (album) {
+      this.selected_album = album
+      this.show_album_details_modal = true
+    },
+
+    artwork_url: function (album) {
+      if (album.images && album.images.length > 0) {
+        return album.images[0].url
+      }
+      return ''
+    }
+  }
+}
+</script>
+
+<style></style>
