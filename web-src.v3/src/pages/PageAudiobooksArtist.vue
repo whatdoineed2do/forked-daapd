6804bc880702ca138551a4bf154f64dcedc17e6a
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2022-04-26 08:17:05 +0100

    ray: [web-src] vue3 from upstream 42e708fb

---
+++ web-src.v3/src/pages/PageAudiobooksArtist.vue
@@ -0,0 +1,99 @@
+<template>
+  <content-with-heading>
+    <template #heading-left>
+      <p class="title is-4">
+        {{ artist.name }}
+      </p>
+    </template>
+    <template #heading-right>
+      <div class="buttons is-centered">
+        <a
+          class="button is-small is-light is-rounded"
+          @click="show_artist_details_modal = true"
+        >
+          <span class="icon"><mdicon name="dots-horizontal" size="16" /></span>
+        </a>
+        <a class="button is-small is-dark is-rounded" @click="play">
+          <span class="icon"><mdicon name="play" size="16" /></span>
+          <span>Shuffle</span>
+        </a>
+      </div>
+    </template>
+    <template #content>
+      <p class="heading has-text-centered-mobile">
+        {{ artist.album_count }} albums
+      </p>
+      <list-albums :albums="albums" />
+      <modal-dialog-artist
+        :show="show_artist_details_modal"
+        :artist="artist"
+        @close="show_artist_details_modal = false"
+      />
+    </template>
+  </content-with-heading>
+</template>
+
+<script>
+import ContentWithHeading from '@/templates/ContentWithHeading.vue'
+import ListAlbums from '@/components/ListAlbums.vue'
+import ModalDialogArtist from '@/components/ModalDialogArtist.vue'
+import webapi from '@/webapi'
+import { GroupByList } from '../lib/GroupByList'
+
+const dataObject = {
+  load: function (to) {
+    return Promise.all([
+      webapi.library_artist(to.params.artist_id),
+      webapi.library_artist_albums(to.params.artist_id)
+    ])
+  },
+
+  set: function (vm, response) {
+    vm.artist = response[0].data
+    vm.albums = new GroupByList(response[1].data)
+  }
+}
+
+export default {
+  name: 'PageAudiobooksArtist',
+  components: { ContentWithHeading, ListAlbums, ModalDialogArtist },
+
+  beforeRouteEnter(to, from, next) {
+    dataObject.load(to).then((response) => {
+      next((vm) => dataObject.set(vm, response))
+    })
+  },
+
+  beforeRouteUpdate(to, from, next) {
+    if (!this.albums.isEmpty()) {
+      next()
+      return
+    }
+    const vm = this
+    dataObject.load(to).then((response) => {
+      dataObject.set(vm, response)
+      next()
+    })
+  },
+
+  data() {
+    return {
+      artist: {},
+      albums: new GroupByList(),
+
+      show_artist_details_modal: false
+    }
+  },
+
+  methods: {
+    play: function () {
+      webapi.player_play_uri(
+        this.albums.items.map((a) => a.uri).join(','),
+        false
+      )
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ web-src.v3/src/pages/SettingsPageWebinterface.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="fd-page-with-tabs">
+    <tabs-settings />
+
+    <content-with-heading>
+      <template #heading-left>
+        <div class="title is-4">Navbar items</div>
+      </template>
+
+      <template #content>
+        <p class="content">Select the top navigation bar menu items</p>
+        <div class="notification is-size-7">
+          If you select more items than can be shown on your screen then the
+          burger menu will disappear.
+        </div>
+        <settings-checkbox
+          category_name="webinterface"
+          option_name="show_menu_item_playlists"
+        >
+          <template #label> Playlists </template>
+        </settings-checkbox>
+        <settings-checkbox
+          category_name="webinterface"
+          option_name="show_menu_item_music"
+        >
+          <template #label> Music </template>
+        </settings-checkbox>
+        <settings-checkbox
+          category_name="webinterface"
+          option_name="show_menu_item_podcasts"
+        >
+          <template #label> Podcasts </template>
+        </settings-checkbox>
+        <settings-checkbox
+          category_name="webinterface"
+          option_name="show_menu_item_audiobooks"
+        >
+          <template #label> Audiobooks </template>
+        </settings-checkbox>
+        <settings-checkbox
+          category_name="webinterface"
+          option_name="show_menu_item_radio"
+        >
+          <template #label> Radio </template>
+        </settings-checkbox>
+        <settings-checkbox
+          category_name="webinterface"
+          option_name="show_menu_item_files"
+        >
+          <template #label> Files </template>
+        </settings-checkbox>
+        <settings-checkbox
+          category_name="webinterface"
+          option_name="show_menu_item_search"
+        >
+          <template #label> Search </template>
+        </settings-checkbox>
+      </template>
+    </content-with-heading>
+
+    <content-with-heading>
+      <template #heading-left>
+        <div class="title is-4">Album lists</div>
+      </template>
+
+      <template #content>
+        <settings-checkbox
+          category_name="webinterface"
+          option_name="show_cover_artwork_in_album_lists"
+        >
+          <template #label> Show cover artwork in album list </template>
+        </settings-checkbox>
+      </template>
+    </content-with-heading>
+
+    <content-with-heading>
+      <template #heading-left>
+        <div class="title is-4">Now playing page</div>
+      </template>
+
+      <template #content>
+        <settings-checkbox
+          category_name="webinterface"
+          option_name="show_composer_now_playing"
+        >
+          <template #label> Show composer </template>
+          <template #info>
+            If enabled the composer of the current playing track is shown on the
+            &quot;now playing page&quot;
+          </template>
+        </settings-checkbox>
+        <settings-textfield
+          category_name="webinterface"
+          option_name="show_composer_for_genre"
+          :disabled="!settings_option_show_composer_now_playing"
+          placeholder="Genres"
+        >
+          <template #label> Show composer only for listed genres </template>
+          <template #info>
+            <p class="help">
+              Comma separated list of genres the composer should be displayed on
+              the &quot;now playing page&quot;.
+            </p>
+            <p class="help">Leave empty to always show the composer.</p>
+            <p class="help">
+              The genre tag of the current track is matched by checking, if one
+              of the defined genres are included. For example setting to
+              <code>classical, soundtrack</code> will show the composer for
+              tracks with a genre tag of &quot;Contemporary Classical&quot;.<br />
+            </p>
+          </template>
+        </settings-textfield>
+      </template>
+    </content-with-heading>
+
+    <content-with-heading>
+      <template #heading-left>
+        <div class="title is-4">Recently added page</div>
+      </template>
+
+      <template #content>
+        <settings-intfield
+          category_name="webinterface"
+          option_name="recently_added_limit"
+        >
+          <template #label>
+            Limit the number of albums shown on the "Recently Added" page
+          </template>
+        </settings-intfield>
+      </template>
+    </content-with-heading>
+  </div>
+</template>
+
+<script>
+import ContentWithHeading from '@/templates/ContentWithHeading.vue'
+import TabsSettings from '@/components/TabsSettings.vue'
+import SettingsCheckbox from '@/components/SettingsCheckbox.vue'
+import SettingsTextfield from '@/components/SettingsTextfield.vue'
+import SettingsIntfield from '@/components/SettingsIntfield.vue'
+
+export default {
+  name: 'SettingsPageWebinterface',
+  components: {
+    ContentWithHeading,
+    TabsSettings,
+    SettingsCheckbox,
+    SettingsTextfield,
+    SettingsIntfield
+  },
+
+  computed: {
+    settings_option_show_composer_now_playing() {
+      return this.$store.getters.settings_option_show_composer_now_playing
+    }
+  }
+}
+</script>
+
+<style></style>
