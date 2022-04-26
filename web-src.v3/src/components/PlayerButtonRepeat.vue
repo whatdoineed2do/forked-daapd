115438094f9996a29b7416e25b32b59b1c82dbec
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2022-04-26 08:17:05 +0100

    ray: [web-src] vue3 from upstream 42e708fb

---
+++ web-src.v3/src/components/PlayerButtonRepeat.vue
@@ -0,0 +1,54 @@
+<template>
+  <a :class="{ 'is-warning': !is_repeat_off }" @click="toggle_repeat_mode">
+    <span class="icon"><mdicon :name="icon_name" :size="icon_size" /></span>
+  </a>
+</template>
+
+<script>
+import webapi from '@/webapi'
+
+export default {
+  name: 'PlayerButtonRepeat',
+
+  props: {
+    icon_size: {
+      type: Number,
+      default: 16
+    }
+  },
+
+  computed: {
+    is_repeat_all() {
+      return this.$store.state.player.repeat === 'all'
+    },
+    is_repeat_single() {
+      return this.$store.state.player.repeat === 'single'
+    },
+    is_repeat_off() {
+      return !this.is_repeat_all && !this.is_repeat_single
+    },
+    icon_name() {
+      if (this.is_repeat_all) {
+        return 'repeat'
+      } else if (this.is_repeat_single) {
+        return 'repeat-once'
+      }
+      return 'repeat-off'
+    }
+  },
+
+  methods: {
+    toggle_repeat_mode: function () {
+      if (this.is_repeat_all) {
+        webapi.player_repeat('single')
+      } else if (this.is_repeat_single) {
+        webapi.player_repeat('off')
+      } else {
+        webapi.player_repeat('all')
+      }
+    }
+  }
+}
+</script>
+
+<style></style>
