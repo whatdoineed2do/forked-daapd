7cdf459a16974739cebe44edb3812159c1bbfce3
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2022-05-21 11:34:56 +0100

    ray: [web-src] sortable tracks

--- web-src.v3/src/pages/PageArtistTracks.vue
+++ web-src.v3/src/pages/PageArtistTracks.vue
@@ -2,12 +2,21 @@
   <div class="fd-page-with-tabs">
     <tabs-music />
 
-    <index-list :index="index_list"></index-list>
+    <index-list :index="tracks.indexList"></index-list>
 
   <div>
     <content-with-heading>
       <template #options>
-        <index-button-list :index="index_list" />
+        <index-button-list :index="tracks.indexList" />
+	<div class="columns">
+	  <div class="column">
+	    <p class="heading" style="margin-bottom: 24px">Sort by</p>
+	    <dropdown-menu
+	      v-model="selected_groupby_option_name"
+	      :options="groupby_option_names"
+	    />
+	  </div>
+	</div>
       </template>
       <template #heading-left>
         <p class="title is-4">
@@ -37,7 +46,7 @@
           >
           | {{ artist.track_count }} tracks
         </p>
-        <list-tracks :tracks="tracks.items" :uris="track_uris" @usermark-updated="usermark_upd" />
+        <list-tracks :tracks="tracks" :uris="track_uris" @usermark-updated="usermark_upd" />
         <modal-dialog-artist
           :show="show_artist_details_modal"
           :artist="artist"
@@ -52,11 +61,14 @@
 <script>
 import ContentWithHeading from '@/templates/ContentWithHeading.vue'
 import TabsMusic from '@/components/TabsMusic.vue'
+import DropdownMenu from '@/components/DropdownMenu.vue'
 import IndexList from '@/components/IndexList.vue'
 import IndexButtonList from '@/components/IndexButtonList.vue'
-import ListTracks from '@/components/ListTracks.vue'
+import ListTracks from '@/components/ListTracksWHeadings.vue'
 import ModalDialogArtist from '@/components/ModalDialogArtist.vue'
 import webapi from '@/webapi'
+import * as types from '@/store/mutation_types'
+import { bySortName, byYear, byRating, GroupByList } from '@/lib/GroupByList'
 
 const dataObject = {
   load: function (to) {
@@ -68,7 +80,7 @@ const dataObject = {
 
   set: function (vm, response) {
     vm.artist = response[0].data
-    vm.tracks = response[1].data.tracks
+    vm.tracks_list = new GroupByList(response[1].data.tracks)
   }
 }
 
@@ -77,6 +89,7 @@ export default {
   components: {
     ContentWithHeading,
     TabsMusic,
+    DropdownMenu,
     IndexList,
     ListTracks,
     IndexButtonList,
@@ -99,21 +112,46 @@ export default {
   data() {
     return {
       artist: {},
-      tracks: { items: [] },
+
+      tracks_list: new GroupByList(),
+
+      groupby_options: [
+        { name: 'Name', options: bySortName('title_sort') },
+        { name: 'Rating', options: byRating('rating') },
+        {
+          name: 'Release date',
+          options: byYear('date_released', {
+            direction: 'asc',
+            defaultValue: '0000'
+          })
+        }
+      ],
 
       show_artist_details_modal: false
     }
   },
 
   computed: {
-    index_list() {
-      return [
-        ...new Set(
-          this.tracks.items.map((track) =>
-            track.title_sort.charAt(0).toUpperCase()
-          )
-        )
-      ]
+    tracks() {
+      const groupBy = this.groupby_options.find(
+        (o) => o.name === this.selected_groupby_option_name
+      )
+      this.tracks_list.group(groupBy.options)
+
+      return this.tracks_list
+    },
+
+    groupby_option_names() {
+      return [...this.groupby_options].map((o) => o.name)
+    },
+
+    selected_groupby_option_name: {
+      get() {
+        return this.$store.state.tracks_sort
+      },
+      set(value) {
+        this.$store.commit(types.TRACKS_SORT, value)
+      }
     },
 
     track_uris() {
